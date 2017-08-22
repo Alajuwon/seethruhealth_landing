41ab688e65e47db1a2bd30b5844c4d7321a122c0
Alajuwon Thomas <spooda7@yahoo.com>
2017-08-22 05:19:51 -0400

    finished main seethru concept

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'see_thru/home'
+  root 'see_thru#home'
 
-  get 'see_thru/about'
+  get 'why_us' => 'see_thru#about'
 
-  get 'see_thru/contact'
+  get 'contact_us' => 'see_thru#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
